3d81e49a47fa79ac7ba35d26a4597267d026f0db
Malvin King <163745886+malvinking@users.noreply.github.com>
2024-06-18 14:15:18 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,21 +57,21 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
                let GIFTED_MD_TEXT = `
-*_Pair Code Connected by Gifted Tech_*
+*_Pair Code Connected by SPEED Tech_*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU CHOOSEN GIFTED-MDv2 』*
+║ *『 WOW YOU CHOOSEN FORNIX-MDv7 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚══════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@giftedtechnexus_
-║❒ *Owner:* _https://wa.me/message/NHCZC5DSOEUXB1_
-║❒ *Repo:* _https://github.com/Giftedmaurice/gifted-mdV2_
+║❒ *Ytube:* _youtube.com/@speedtechi
+║❒ *Owner:* _https://wa.me/message/263780166288_
+║❒ *Repo:* _https://github.com/malvinking/Fornix-MD_
 ║❒ *WaGroup:* _https://chat.whatsapp.com/L0ctUUVLlsrFYwBHApKfew_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaJmfmTDJ6H7CmuBss0o_
-║❒ *Plugins:* _https://github.com/Giftedmaurice/gifted-bot-md-plugins_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029Vac8SosLY6d7CAFndv3Z
+║❒ *Plugins:* _https://github.com/Giftedmaurice/Fornix-MD-plugins_
 ╚══════════════════════╝ 
 _____________________________________
 
